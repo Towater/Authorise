a1d955ac08490ab7f134d2066f79061c47d2e695
zen <aries__y@163.com>
2019-10-30 15:38:48 +0800

    init repository

---
+++ src/main/java/com/xplay/GithubAuthoriseLoginApplication.java
@@ -0,0 +1,13 @@
+package com.xplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAuthoriseLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubAuthoriseLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xplay/GithubAuthoriseLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAuthoriseLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
